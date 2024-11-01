584bef030810445163f5fd47dd1487195f867779
minobol <120034142+minobol@users.noreply.github.com>
2024-11-01 16:10:10 +0300

    Initial Commit

---
+++ Sonet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sonet
+//
+//  Created by MacBook on 01.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
